c20007fc992ca55c1b2e4995cd73738985115cd5
jenish-prog <jenish112005@gmail.com>
2026-04-15 16:27:27 +0530

    feat: add react-snap for pre-rendering, update sitemap URLs to include www, and improve hydration in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { hydrateRoot, createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root');
+const element = (
   <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+if (container.hasChildNodes()) {
+  hydrateRoot(container, element);
+} else {
+  createRoot(container).render(element);
+}
